8878928afdb902fe69bc83a0f82c8e2bdceaaaba
Dev Rathore <devrathore653@gmail.com>
2026-01-24 01:13:19 +0530

    feat: add mock profile data

---
+++ app/src/types/Profile.ts
@@ -0,0 +1,11 @@
+export interface Profile {
+  id: string;
+  name: string;
+  age: number;
+  photoUrl: string;
+  isVerified: true;
+  city: string;
+  income: string;
+  job: string;
+  bio: string;
+}
